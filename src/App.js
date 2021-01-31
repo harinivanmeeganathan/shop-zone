b4b3bb2aa5a51c0b18628ec91a6c60c8ba6e65f4
harinivanmeeganathan <harinivanmeeganathan@gmail.com>
2021-01-31 23:06:44 +0530

    implemented firebase utils and ability to store authenticated

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import {Switch,Route} from 'react-router-dom'; 
 
 import Header from './components/header/header.component';
-import SignIn from './components/sign-in/sign-in.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import {auth} from '../src/components/firebase/firebase.utils';
+import {auth,createUserProfileDocument} from '../src/components/firebase/firebase.utils';
 
 class App extends React.Component{
   constructor() {
@@ -21,10 +21,23 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -39,7 +52,7 @@ class App extends React.Component{
       <Switch>
         <Route exact path = '/' component = {HomePage}/>
         <Route  path = '/shop' component = {ShopPage}/>
-        <Route  path = '/signin' component = {SignIn}/>
+        <Route  path = '/signin' component = {SignInAndSignUpPage}/>
 
         </Switch>
     </div>
--- src/components/firebase/firebase.utils.js
+++ src/components/firebase/firebase.utils.js
@@ -13,7 +13,33 @@ appId: "1:1004142887455:web:db52695544c0c900d34b2a",
 measurementId: "G-SYLV9H5EL3"
 };
 
+
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+    const snapShot = await userRef.get();
+  
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+  
+    return userRef;
+  };
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
